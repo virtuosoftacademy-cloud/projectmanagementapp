295501fb8ed998baab413dcca57810cbfb936a28
Virtuosoft <virtuosoftacademy@gmail.com>
2026-04-21 19:01:08 +0500

    workspace

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/sidebar"
 import { CirclePlusIcon, MailIcon } from "lucide-react"
 import Link from "next/link"
+import { AddWorkspace } from "./workspace/AddWorkspace"
 
 export function NavMain({
   items,
@@ -24,15 +25,15 @@ export function NavMain({
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
         <SidebarMenu>
-          <SidebarMenuItem className="flex items-center gap-2">
-            <SidebarMenuButton
-              tooltip="Quick Create"
-              className="min-w-8 bg-primary text-primary-foreground duration-200 ease-linear hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground"
+          <SidebarMenuItem className="flex items-center justify-between gap-2">
+            <div
+              className="flex p-2 rounded gap-3 min-w-8 bg-primary text-primary-foreground duration-200 ease-linear hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground"
             >
               <CirclePlusIcon
               />
-              <span>Quick Create</span>
-            </SidebarMenuButton>
+              <AddWorkspace/>
+              </div>
+              {/* <span>Add Workspace</span> */}
             <Button
               size="icon"
               className="size-8 group-data-[collapsible=icon]:opacity-0"
---
+++ components/workspace/AddWorkspace.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Field, FieldGroup } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function AddWorkspace() {
+  return (
+  <Dialog>
+  <DialogTrigger>Add Workspace</DialogTrigger>
+  <DialogContent>
+    <DialogHeader>
+      <DialogTitle>Are you absolutely sure?</DialogTitle>
+      <DialogDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </DialogDescription>
+    </DialogHeader>
+  </DialogContent>
+</Dialog>
+  )
+}
